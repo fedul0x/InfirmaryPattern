5cf8ec7d57235e49a6ecc6d99a95c081f924a8bf
Alexey Ivashin <ivashin.alexei@gmail.com>
2012-12-23 03:37:21 +0400

    Correct some errors in POJO objects and into *.hbm.xml files

--- src/ip/InfirmaryPattern.java
+++ src/ip/InfirmaryPattern.java
@@ -4,6 +4,11 @@
  */
 package ip;
 
+import ip.dataaccess.Agent;
+import ip.dataaccess.HibernateUtil;
+import ip.dataaccess.StaffPosition;
+import org.hibernate.classic.Session;
+
 /**
  *
  * @author Ivashin Alexey <ivashin.alexei@gmail.com>
@@ -14,6 +19,17 @@ public class InfirmaryPattern {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+//        Agent agent = new Agent(0, "Last", "Get", "Get", "Get", null)
+//        agent.setId(0);
+        StaffPosition sp = new StaffPosition(4);
+        StaffPosition sp1 = new StaffPosition(5);
+        StaffPosition sp2 = new StaffPosition(6);
+        
+        Session openSession = HibernateUtil.getSessionFactory().openSession();
+        openSession.beginTransaction();
+        openSession.save(sp);
+        openSession.save(sp1);
+        openSession.save(sp2);
+        openSession.getTransaction().commit();
     }
 }
