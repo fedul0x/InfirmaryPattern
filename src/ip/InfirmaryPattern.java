fd2023ca3acb77e408903e22223bd804ee6d15b4
Alexey Ivashin <ivashin.alexei@gmail.com>
2012-12-23 19:59:04 +0400

    Correct some errors in data access

--- src/ip/InfirmaryPattern.java
+++ src/ip/InfirmaryPattern.java
@@ -21,15 +21,13 @@ public class InfirmaryPattern {
     public static void main(String[] args) {
 //        Agent agent = new Agent(0, "Last", "Get", "Get", "Get", null)
 //        agent.setId(0);
-        StaffPosition sp = new StaffPosition(4);
-        StaffPosition sp1 = new StaffPosition(5);
-        StaffPosition sp2 = new StaffPosition(6);
-        
+        StaffPosition sp = new StaffPosition(7L);
+
         Session openSession = HibernateUtil.getSessionFactory().openSession();
         openSession.beginTransaction();
-        openSession.save(sp);
-        openSession.save(sp1);
-        openSession.save(sp2);
+        for (StaffPosition staffPosition : StaffPosition.AllStaffPositions) {
+            openSession.saveOrUpdate(staffPosition);
+        }
         openSession.getTransaction().commit();
     }
 }
